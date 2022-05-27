e81aaa64963c2c9fb6d25e52b4aa48f689f98ad2
rgiinther <r.giin95@gmail.com>
2022-05-27 10:55:01 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./dist/index.html');
+
+// TODO: Create an array of questions for user input
+// TODO: Create a function to write HTML file
+// TODO: Create a function to initialize app
+// Function call to initialize app
